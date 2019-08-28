f1cf19072b80e9ced14d0f898c86fa9a14a08507
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-08-28 17:17:23 +0300

    SqlLogTransactionManagement

---
+++ SqlLogTransactionManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using SqlLogTransactionManagement.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SqlLogTransactionManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+
+                List<Employee> empList = db.Employee.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
